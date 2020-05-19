6e1dafc839a36360fec0718bcbd4b9628db52362
Giovanni <44929830+Gioovannii@users.noreply.github.com>
2020-05-19 12:13:34 +0800

    Set image for view Background than test to press in text field

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -13,12 +13,16 @@ class WeatherViewController: UIViewController {
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var searchTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func searchedPressed(_ sender: UIButton) {
+        guard let searchTextField = searchTextField.text else { return }
+        print(searchTextField)
+    }
 }
 
